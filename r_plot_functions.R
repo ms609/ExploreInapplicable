7db9522065eaff4c7d1321839f8ba151f816deec
Martin R. Smith <martins@gmail.com>
2017-10-03 17:11:20 +0100

    Split functions into their own file

--- r_plot_functions.R
+++ r_plot_functions.R
@@ -46,31 +46,3 @@ PlotTreeSpace <- function (pcs, nTrees, legendPos = 'bottomleft') {
     lines(x[iTrees][convexHull], y[iTrees][convexHull], col=treePalette[i])
   }
 }
-
-
-
-tntDirectories <- c('ambiguous', 'ambigAbsent', 'extraState')
-rDirectories <- c('inapplicable')
-
-
-
-nexusFiles <- list.files('matrices', pattern='.*\\.nex$')
-nexusName <- nexusFiles[1]
-
-trees <- c(lapply(tntDirectories, readTntTrees, nexusName=nexusName), 
-              lapply(rDirectories, readRTrees, nexusName=nexusName))
-nTrees <- vapply(trees, length, integer(1))
-flatTrees <- unlist(trees, recursive=FALSE)
-treeTitles <- paste(rep(c(tntDirectories, rDirectories), nTrees), unlist(sapply(nTrees, seq_len)))
-treeCol <- paste(rep(treePalette, nTrees))
-treePCh <- rep(plotChars, nTrees)
-
-# Crude analysis inspired by http://www.kmeverson.org/blog/visualizing-tree-space-in-r
-allDistances <- RFDistances(flatTrees); 
-
-PlotTreeSpace(pcoa(allDistances), nTrees)
-dev.copy2pdf(file=paste0('treeSpaces/', nexusName, '.pdf', collapse=''))
-
-# Really we want to something more sophisticated: e.g.
-# HILLIS, D. M., HEATH, T. A., JOHN, K. St. and ANDERSON, F. 2005. Analysis and Visualization of Tree Space. Systematic Biology, 54, 471–482.
-# or WILGENBUSCH, J. C., HUANG, W. and GALLIVAN, K. A. 2017. Visualizing phylogenetic tree landscapes. BMC Bioinformatics, 18, 85.
