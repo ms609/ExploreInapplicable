7dbc4a3072525068b8b646e958f3c1b3fc5e4a47
Martin R. Smith <martins@gmail.com>
2017-10-04 12:30:35 +0100

    Add Aria 2015

--- r_search.R
+++ r_search.R
@@ -14,18 +14,18 @@ best <- ape::root(ape::nj(phangorn::dist.hamming(phyData)), names(rawData)[1], r
 attr(best, 'pscore') <- 1e+7
 bestScore <- 1e+7
 bestHits <- 0
-verbose <- 0
 
 
 maxHitMenu <- c(2:16, 25, 40, 60, 100, 150, 200)
 
 
+verbose <- 0
 for (i in 1:15000) {
   started <- Sys.time()
   cat ("\n > Setting maxHits =", maxHits <- sample(maxHitMenu, 1))
-  best <- Ratchet(best, phyData, maxIt=1e+7, maxIter=3e+7, maxHits=maxHits, k = 1, verbosity=verbose)
+  best <- Ratchet(best, phyData, maxIt=1e+7, maxIter=1.6e+5, maxHits=maxHits, k = 1, verbosity=verbose) # Maxiter of 3e+7 was causing unhappy delays when there was a single best tree at a local optimum.
   secsTaken <- as.numeric(difftime(Sys.time(), started, units='secs'))
-  verbose <- if (secsTaken > 5) 3 else if (secsTaken > 3) 1 else if (secsTaken < 2) 0 else verbose
+  #verbose <- if (secsTaken > 5) 3 else if (secsTaken > 3) 1 else if (secsTaken < 2) 0 else verbose
   plot (best, cex=0.95, main=attr(best, 'pscore'))
   treeScore <- attr(best, 'pscore')
   if (treeScore < bestScore) {
