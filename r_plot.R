fad07de3a05f8c527f320c90f101bc0ff99fc892
Martin R. Smith <martins@gmail.com>
2017-10-05 11:40:38 +0100

    Handle single MPT scenarios with , drop=FALSE

--- r_plot.R
+++ r_plot.R
@@ -9,11 +9,11 @@ nexusFiles <- list.files('matrices', pattern='.*\\.nex$');# nexusFiles
 for (nexusName in nexusFiles) {
   nexusRoot <- gsub('.nex', '', nexusName); cat("\nEvaluating", nexusRoot, "...\n")
   
-  if (nexusRoot == 'Aguado2009') {
-    cat (" ! Problem calculating quartet distaances: Error in 1:(zero.eig[1] - 1): NA/NaN argument")
+  if (nexusRoot %in% c('Aguado2009')) {
+    cat (" ! Manual override")
     next
   }
-  if (file.exists(paste0('treeSpaces/', nexusName, '.qt.png', collapse='')) && !OVERWRITE) {
+  if (file.exists(paste0('treeSpaces/', nexusRoot, '.qt3.png', collapse='')) && !OVERWRITE) {
     cat(" - Results already exist.\n")
     next
   }
@@ -45,21 +45,21 @@ for (nexusName in nexusFiles) {
   extraSteps <- scores - matrix(minScores, nrow(scores), ncol(scores), byrow=TRUE)
   
   # Plot tree scores
-  oldPar <- par(mfrow=c(2,2), bg='white')
+  oldPar <- par(mfrow=c(2, 2), bg='white')
   for (dirPath in allDirectories) {
-    dirScores <- extraSteps[dirTrees[[dirPath]], ]
+    dirScores <- extraSteps[dirTrees[[dirPath]], , drop=FALSE]
     dirBreaks <- (min(dirScores) - 1):max(dirScores) + 0.5
     dirCol <- treePalette[dirPath]
     otherDirectories <- which(allDirectories != dirPath)
     
-    yMax <- max(apply(dirScores[, otherDirectories], 2, function (x) max(table(x))))
+    yMax <- max(apply(dirScores[, otherDirectories, drop=FALSE], 2, function (x) max(table(x))))
     hist(0, breaks=dirBreaks, border='#ffffffff', ylim=c(0, yMax), axes=FALSE, font.main=1, cex.main=1,
          main=paste0("Trees on ", dirPath, ' island (', nexusRoot, ")"), col.main=dirCol, xlab="Steps longer than best tree") # Set up blank histogram
     axis(1, col=dirCol)
     axis(2, col=dirCol)
     
     for (i in otherDirectories) {
-      hist(dirScores[, i], add=TRUE, breaks=dirBreaks,
+      hist(dirScores[, i, drop=FALSE], add=TRUE, breaks=dirBreaks,
            border=treePalette[i], col=paste0(treePalette[i], '99', collapse=''))
     }
   }
@@ -77,21 +77,26 @@ for (nexusName in nexusFiles) {
   rfDistances <- RFDistances(flatTrees);
   cat(" - Calculating quartet disances...\n")
   qtDistances <- QuartetDistances(flatTrees);
+  ambiguousTrees <- 1:nTrees[1]
     
   rfSpace <- pcoa(rfDistances)
   PlotTreeSpace(rfSpace, nTrees, legendPos='bottomright', rfTitleText)
-  dev.copy(svg, file=paste0('treeSpaces/', nexusName, '.rf4.svg', collapse='')); dev.off()
-  dev.copy(png, file=paste0('treeSpaces/', nexusName, '.rf4.png', collapse='')); dev.off()
-  rf3 <- pcoa(rfDistances[-(1:nTrees[1]), -(1:nTrees[1])])
+  dev.copy(svg, file=paste0('treeSpaces/', nexusRoot, '.rf4.svg', collapse='')); dev.off()
+  dev.copy(png, file=paste0('treeSpaces/', nexusRoot, '.rf4.png', collapse='')); dev.off()
+  rf3 <- modifiedPcoa(rfDistances[-ambiguousTrees, -ambiguousTrees])
   PlotTreeSpace3(rf3, nTrees, legendPos='bottomright', rfTitleText)
-  dev.copy(svg, file=paste0('treeSpaces/', nexusName, '.rf3.svg', collapse='')); dev.off()
-  dev.copy(png, file=paste0('treeSpaces/', nexusName, '.rf3.png', collapse='')); dev.off()
-
+  dev.copy(svg, file=paste0('treeSpaces/', nexusRoot, '.rf3.svg', collapse='')); dev.off()
+  dev.copy(png, file=paste0('treeSpaces/', nexusRoot, '.rf3.png', collapse='')); dev.off()
   cat(" - Printed RF SVG & PNG.\n")
-  qtSpace <- pcoa(qtDistances)
+
+  qtSpace <- modifiedPcoa(qtDistances)
   PlotTreeSpace(qtSpace, nTrees, legendPos='bottomleft', qtTitleText)
-  dev.copy(svg, file=paste0('treeSpaces/', nexusName, '.qt.svg', collapse='')); dev.off()
-  dev.copy(png, file=paste0('treeSpaces/', nexusName, '.qt.png', collapse='')); dev.off()
+  dev.copy(svg, file=paste0('treeSpaces/', nexusRoot, '.qt4.svg', collapse='')); dev.off()
+  dev.copy(png, file=paste0('treeSpaces/', nexusRoot, '.qt4.png', collapse='')); dev.off()
+  qt3 <- modifiedPcoa(qtDistances[-ambiguousTrees, -ambiguousTrees])
+  PlotTreeSpace3(qt3, nTrees, legendPos='bottomleft', qtTitleText)
+  dev.copy(svg, file=paste0('treeSpaces/', nexusRoot, '.qt3.svg', collapse='')); dev.off()
+  dev.copy(png, file=paste0('treeSpaces/', nexusRoot, '.qt3.png', collapse='')); dev.off()
   cat("- Printed Quartet SVG & PNG.\n\n")
 }
 
