c10b3fa9e635bf1c750773a1f3815edece0718a1
Martin R. Smith <martins@gmail.com>
2017-10-09 16:58:58 +0100

    No obvious trends...

--- r_analysis.R
+++ r_analysis.R
@@ -1,31 +1,37 @@
 source('rPlot/functions.R')
 source('rPlot/definitions.R')
 
-### Analyse precision:
-### Island sizes
-csvs <- list.files('islandCounts', pattern='.*\\.csv$');# nexusFiles
+props <- read.csv('matrixProperties.csv', header=TRUE)
+countDims <- vapply(rownames(props), MatrixProperties, integer(6))
 
-islandSizes <- vapply(csvs, function (csvFile) {
-  content <- read.csv(csvFile)
+validReads <- props$nTax == countDims['nTax',]
+props[validReads, c('nTokens', 'chars_na', 'tokens_na', 'tokens_amb')] <- 
+  t(countDims[c('nTokens', 'nInapp', 'inappTokens', 'ambigTokens'), validReads])
+props$prop_na <- props$tokens_na / props$nTokens
+props$prop_chars_na <- props$chars_na / props$nChars
+rownames(props) <- props$matrix
+
+### Analyse precision:
+### Numbers of MPTs
+csvs <- list.files('islandCounts', pattern='.*\\.csv$');
+
+mptCount <- vapply(csvs, function (csvFile) {
+  content <- read.csv(paste0('islandCounts/', csvFile))
   table(content$X)
 }, double(4))[-1, ]
+colnames(mptCount) <- gsub('\\.csv', '', colnames(mptCount))
+mpts <- t(mptCount)
+props[, c('ambigMPTs', 'exstMPTs', 'inappMPTs')] <- NA
+props[colnames(mptCount), c('ambigMPTs', 'exstMPTs', 'inappMPTs')] <- t(mptCount)
 
-ncol(islandSizes)
-sum(islandSizes[3, ] == islandSizes[2, ])
-sum(islandSizes[3, ] == islandSizes[1, ])
-sum(islandSizes[2, ] == islandSizes[1, ])
-sum(islandSizes[3, ] > islandSizes[2, ])
-sum(islandSizes[3, ] > islandSizes[1, ])
-sum(islandSizes[2, ] > islandSizes[1, ])
+
+
 
 nexusFiles <- list.files('matrices', pattern='.*\\.nex$');# nexusFiles
 
-precision <- matrix(NA, 4, length(nexusFiles))
-colnames(precision) <- nexusFiles; rownames(precision) <- allDirectories
-differences <- matrix(NA, 4, length(nexusFiles))
-colnames(differences) <- nexusFiles; rownames(differences) <- c('diffAmbExt', 'diffAmbInap', 'diffExtrInap', 'allCons')
 for (nexusName in nexusFiles) {
-  nexusRoot <- gsub('.nex', '', nexusName); 
+  nexusRoot <- gsub('.nex', '', nexusName)
+  if (!any(is.na(props[nexusRoot, c('consNodes_ambig', 'consNodes_all')]))) next
   cat("\nEvaluating", nexusRoot, "...\n")
  
   rTrees <- lapply(rDirectories, readRTrees, nexusName=nexusName)
@@ -34,36 +40,27 @@ for (nexusName in nexusFiles) {
     next
   }
   trees <- c(lapply(tntDirectories, readTntTrees, nexusName=nexusName), rTrees)
+  cat(" - Trees loaded. Calculating consensus trees: ")
   consensi <- lapply(trees, consensus)
-  
-  cons23 <- consensus(consensi[[2]], consensi[[3]])$Nnode
-  cons24 <- consensus(consensi[[2]], consensi[[4]])$Nnode
-  cons34 <- consensus(consensi[[4]], consensi[[3]])$Nnode
-  allCons <- consensus(consensi[2:4])$Nnode
-  differences[, nexusName] <- c(cons23, cons24, cons34, allCons)
-  
   nNodes <- vapply(consensi, function (tr) tr$Nnode, double(1))
-  precision[, nexusName] <- nNodes
+  cat("done.\n")
+  props[nexusRoot, c('consNodes_ambig', 'consNodes_exst', 'consNodes_inapp')] <- nNodes[2:4]
+  
+  props[nexusRoot, 'consNodes_ambig_exst'] <- consensus(consensi[[2]], consensi[[3]])$Nnode
+  props[nexusRoot, 'consNodes_ambig_inapp']  <- consensus(consensi[[2]], consensi[[4]])$Nnode
+  props[nexusRoot, 'consNodes_exst_inapp'] <- consensus(consensi[[4]], consensi[[3]])$Nnode
+  props[nexusRoot, 'consNodes_all'] <- consensus(consensi[[4]], consensi[[3]], consensi[[2]])$Nnode
 }
 
-precisions <- precision[-1, !is.na(precision[1, ])]
+write.csv(props, 'matrixProperties.csv')
 
-ncol(precisions)
-sum(precisions[3, ] == precisions[2, ])
-sum(precisions[3, ] == precisions[1, ])
-sum(precisions[2, ] == precisions[1, ])
-sum(precisions[3, ] >  precisions[2, ])
-sum(precisions[3, ] >  precisions[1, ])
-sum(precisions[2, ] >  precisions[1, ])
+plot(I(consNodes_exst_inapp / consNodes_inapp) ~ prop_na, data=props)
+plot(I(consNodes_inapp / nTax) ~ prop_na, data=props)
+plot(I(consNodes_exst_inapp / consNodes_inapp) ~ prop_chars_na, data=props)
+plot(I(consNodes_exst_inapp / consNodes_inapp) ~ nTokens, data=props)
 
-agreements <- differences[, !is.na(differences[1, ])]
-agreements
+plot(I(inappMPTs) ~ prop_na, data=props)
+plot(I(ambigMPTs / ) ~ prop_na, data=props)
 
-nodesLostByAddingAmbig <- (precisions['inapplicable', ] - agreements['diffAmbInap', ] ) /  precisions['inapplicable', ]
-nodesLostByAddingExtSt <- (precisions['inapplicable', ] - agreements['diffExtrInap', ]) / precisions['inapplicable', ] 
 
-1 - mean(nodesLostByAddingAmbig)
-1 - mean(nodesLostByAddingExtSt)
-
-ambigNodesListByAddingExtSt <- (precisions['ambigAbsent', ] - agreements['diffAmbExt', ]) / precisions['ambigAbsent', ] 
-1-mean(ambigNodesListByAddingExtSt)
+### Analyse accuracy:
