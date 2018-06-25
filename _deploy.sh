8d60a77a2da850c341725cba81745f47891c9a6b
Martin Smith <martins@gmail.com>
2018-06-25 14:12:34 +0100

    Clone repo before pushing

--- _deploy.sh
+++ _deploy.sh
@@ -6,6 +6,9 @@ set -e
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 git config --global user.email "martins@gmail.com"
 git config --global user.name "Martin Smith"
-git add --all *.html
+git clone -b origin https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git git-files
+cd git-files
+cp -r ../*.html ./
+git add --all *
 git commit -m"[TRAVIS:] Update compiled html files" || true
-git push -q
+git push -q origin
