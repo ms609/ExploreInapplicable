94da29114d35cfb156e555cd3b8c6ee8b37ba684
Martin Smith <martins@gmail.com>
2018-11-13 09:41:57 +0000

    Use 'master' instead of 'origin'?

--- _deploy.sh
+++ _deploy.sh
@@ -4,9 +4,9 @@ set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-git config --global user.email "martins@gmail.com"
+git config --global user.email "martin.smith@durham.ac.uk"
 git config --global user.name "Martin Smith"
-git clone -b origin https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git git-files
+git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git git-files
 cd git-files
 cp -r ../*.html ./
 git add --all *
