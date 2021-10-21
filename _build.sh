da01bb220be84f51887045a45e20a8b9e93997cf
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-10-21 12:33:58 +0100

    Remove files

--- _build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-Rscript -e "devtools::install_github('ropensci/rcrossref')" # Need version 0.8.1 or higher
-Rscript -e "devtools::install_github('ms609/TreeSearch')" # Need version > 0.1.2
-Rscript -e "rmarkdown::render('r_dataset_details.Rmd', 'bookdown::gitbook')"
